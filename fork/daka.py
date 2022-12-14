06f8e5180103560363aadef9f7e0e87d95450cc9
wh201906 <wh201906@yandex.com>
2022-12-14 21:53:09 +0800

    Misc
    Flush output
    Format code with Black
    Trigger the runner after every push

--- fork/daka.py
+++ fork/daka.py
@@ -17,10 +17,15 @@ def isLocal():
     return runner == "Local"
 
 
+def localPrint(*values):
+    if isLocal():
+        print(*values, flush=True)
+
+
 def getRunner():
     global runner
     try:
-        runner = str(environ['DAKA_RUNNER'])
+        runner = str(environ["DAKA_RUNNER"])
         if runner != "GitHub" and runner != "Gitee":
             runner = "Local"
     except KeyError:
@@ -29,15 +34,16 @@ def getRunner():
 
 def check(sessionid: str):
     headers = {
-        'Content-Type':
-        'application/json',
-        'X-Auth-Token':
-        sessionid,
-        'User-Agent':
-        'Mozilla/5.0 (Linux; Android 11; Pixel 4 XL Build/RQ3A.210705.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 Al'
-        + 'iApp(Din' + 'gTa' + 'lk/6.5.45) com.al' + 'iba' + 'ba.android.rim' +
-        'et/26284409 Channel/700159 language/zh-CN UT4' +
-        'Aplus/0.2.25 colorScheme/light'
+        "Content-Type": "application/json",
+        "X-Auth-Token": sessionid,
+        "User-Agent": "Mozilla/5.0 (Linux; Android 11; Pixel 4 XL Build/RQ3A.210705.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 Al"
+        + "iApp(Din"
+        + "gTa"
+        + "lk/6.5.45) com.al"
+        + "iba"
+        + "ba.android.rim"
+        + "et/26284409 Channel/700159 language/zh-CN UT4"
+        + "Aplus/0.2.25 colorScheme/light",
     }
     url = "https://sk" + "l.h" + "du.edu.cn/api/pun" + "ch"
     data = {
@@ -49,54 +55,49 @@ def check(sessionid: str):
         "health" + "Code": 0,
         "health" + "Report": 0,
         "current" + "Living": 0,
-        "last1" + "4days": 0
+        "last1" + "4days": 0,
     }
 
     for i in range(3):
         try:
             res = httpx.post(url, json=data, headers=headers, timeout=30)
-            if isLocal():
-                print(res.status_code, res.text)
+            localPrint(res.status_code, res.text)
             if res.status_code == 200:
                 return True
             elif res.status_code == 400 and "已经打卡" in res.text:
-                print("Already checked in")
+                print("Already checked in", flush=True)
                 return True
             else:
-                print(f"#{i + 1} Failed")
+                print(f"#{i + 1} Failed", flush=True)
         except Exception as e:
-            print(f"#{i + 1} Failed")
-            if isLocal():
-                print(repr(e))
+            print(f"#{i + 1} Failed", flush=True)
+            localPrint(repr(e))
     return False
 
 
 def getSessionId(username: str, password: str):
-    print("Getting session ID")
+    print("Getting session ID", flush=True)
     options = Options()
     options.headless = not isLocal()
     if runner == "GitHub":
-        options.set_preference('network.trr.mode', 2)
-        options.set_preference('network.trr.uri',
-                               'https://dns.google/dns-query')
+        options.set_preference("network.trr.mode", 2)
+        options.set_preference("network.trr.uri", "https://dns.google/dns-query")
     driver = webdriver.Firefox(options=options)
     wait = WebDriverWait(driver, 10, 0.5)
     try:
         driver.get("https://ca" + "s.hd" + "u.edu.cn/c" + "as/login")
         wait.until(EC.presence_of_element_located((By.ID, "un")))
         wait.until(EC.presence_of_element_located((By.ID, "pd")))
-        wait.until(
-            EC.presence_of_element_located((By.ID, "index_log" + "in_btn")))
-        print("Login page loaded")
+        wait.until(EC.presence_of_element_located((By.ID, "index_log" + "in_btn")))
+        print("Login page loaded", flush=True)
         driver.find_element(By.ID, "un").clear()
         driver.find_element(By.ID, "un").send_keys(username)
         driver.find_element(By.ID, "pd").clear()
         driver.find_element(By.ID, "pd").send_keys(password)
         driver.find_element(By.ID, "index_log" + "in_btn").click()
     except Exception as e:
-        print("Cannot access ca" + "s.hdu.edu.cn")
-        if isLocal():
-            print(repr(e))
+        print("Cannot access ca" + "s.hdu.edu.cn", flush=True)
+        localPrint(repr(e))
         return ""
 
     loginFailed = False
@@ -104,43 +105,46 @@ def getSessionId(username: str, password: str):
     try:
         wait.until(
             EC.presence_of_element_located(
-                (By.XPATH, "//span[@id='total_num' or @id='errormsg']")))
+                (By.XPATH, "//span[@id='total_num' or @id='errormsg']")
+            )
+        )
         if len(driver.find_elements(By.ID, "errormsg")) > 0:
             loginFailed = True
         else:
             loginFailed = False
-    except Exception as e: # timeout
-        print("Warning: Cannot detect login state by element")
-        if isLocal():
-            print(repr(e), driver.current_url)
+    except Exception as e:  # timeout
+        print("Warning: Cannot detect login state by element", flush=True)
+        localPrint(repr(e), driver.current_url)
         # Try to get the session id anyway
         loginFailed = False
 
     if loginFailed:
         driver.quit()
-        print("Failed to log in")
+        print("Failed to log in", flush=True)
         return ""
     else:
         sessionId = readSessionId(driver, wait)
         if sessionId is not None and sessionId != "":
             result = sessionId
         else:
-            print("Cannot get SessionId")
+            print("Cannot get SessionId", flush=True)
             result = ""
         driver.quit()
         return result
 
 
 def readSessionId(driver, wait):
-    driver.get("https://sk" + "l.h" + "duhe" + "lp.com/pas" +
-               "scard.html#/pas" + "scard")
+    driver.get(
+        "https://sk" + "l.h" + "duhe" + "lp.com/pas" + "scard.html#/pas" + "scard"
+    )
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "nucleic-acid")))
-    print("Passcard page loaded")
+    print("Passcard page loaded", flush=True)
 
     for retryCnt in range(10):
         time.sleep(1)
         sessionId = driver.execute_script(
-            "return window.localStorage.getItem('sessionId')")
+            "return window.localStorage.getItem('sessionId')"
+        )
         if sessionId is not None and sessionId != "":
             return sessionId
     return ""
@@ -149,20 +153,20 @@ def readSessionId(driver, wait):
 if __name__ == "__main__":
 
     getRunner()
-    print("Runner:", runner)
+    print("Runner:", runner, flush=True)
 
     if isLocal():
         from secret import *
 
-    sessionId = getSessionId(environ['MY_SECRET_USERNAME'],
-                             environ['MY_SECRET_PASSWORD'])
+    sessionId = getSessionId(
+        environ["MY_SECRET_USERNAME"], environ["MY_SECRET_PASSWORD"]
+    )
     if sessionId == "":
         sys.exit(1)
-    if isLocal():
-        print("Session Id:", sessionId)
+    localPrint("Session Id:", sessionId)
 
     if not check(sessionId):
         sys.exit(1)
     else:
-        print("OK")
+        print("OK", flush=True)
         sys.exit(0)
--- src/delay.py
+++ src/delay.py
@@ -1,6 +1,6 @@
 from random import randint
 from time import sleep
 
-print("Random delay starts")
+print("Random delay starts", flush=True)
 sleep(randint(10, 5 * 60))
-print("Random delay ends")
+print("Random delay ends", flush=True)
